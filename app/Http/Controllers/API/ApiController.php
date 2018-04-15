2e2d59913c9573336ab9655bc4982198d3297e0a
Allen <allenfedd@gmail.com>
2018-04-15 14:21:03 +0800

    Add api response

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Support\Response\Response;
 
 class ApiController extends Controller
 {
+    /**
+     * @var \App\Support\Response\Response
+     */
+    public function response(){
+        return app(Response::class);
+    }
 }
